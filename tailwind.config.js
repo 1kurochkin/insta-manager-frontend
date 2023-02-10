2b6bfc5d974bb7e6c4d2ce24f493e1adc7b08034
Bob Developer <bobdeveloperbob@gmail.com>
2023-02-09 23:46:13 -0500

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: { 
+        primary: 'white',
+        secondary: 'rgb(42 230 16)',
+        background: 'rgb(9 7 58)'
+      }
+    },
+    screens: {
+      sm: {'max':'650px'},
+      md: {'max':'875px'},
+      lg: {'max':'1024px'},
+      xl: {'max':'1280px'},
+      "2xl": {'max':'1536px'}
+    }
+  },
+  plugins: [],
+}
